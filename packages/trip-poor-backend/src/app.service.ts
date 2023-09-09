fd870212ac4663c7ee1776b116c1fea7866ad119
hugo.p <jongyeonpark.dev@gmail.com>
2023-09-09 21:31:38 +0900

    Added env files

--- packages/trip-poor-backend/src/app.service.ts
+++ packages/trip-poor-backend/src/app.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly configService: ConfigService) {}
+  
+  getHello(): Object {
+    const port = this.configService.get<number>("PORT", 0);
+    const env = this.configService.get<string>("NODE_ENV", "");
+    const testval = this.configService.get<string>("TEST_VAL", "");
+    const path = `${__dirname}/config/env/.${process.env.NODE_ENV}.env`;
+    return {
+      port,
+      env,
+      testval,
+      path
+    };
   }
 }
