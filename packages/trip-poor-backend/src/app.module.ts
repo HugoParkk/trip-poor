eb81d4c85fec8b9e19647c6b8886b063d190c7d8
hugopark <jongyeonpark.dev@gmail.com>
2023-09-11 11:10:39 +0900

    added auth module

--- packages/trip-poor-backend/src/app.module.ts
+++ packages/trip-poor-backend/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -9,6 +10,7 @@ import { ConfigModule } from '@nestjs/config';
       isGlobal: true,
       envFilePath: `./config/env/.${process.env.NODE_ENV}.env`,
     }),
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
