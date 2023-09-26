08c6989adf589a00abb9682f3322b882cf5a116d
hugopark <jongyeonpark.dev@gmail.com>
2023-09-26 11:12:19 +0900

    board post logic add

--- packages/trip-poor-backend/src/board/board.service.ts
+++ packages/trip-poor-backend/src/board/board.service.ts
@@ -1,7 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { BoardEntity } from 'src/entities/boardEntity.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class BoardService {
+  private readonly logger = new Logger(BoardService.name);
+
+  constructor(
+    @InjectRepository(BoardEntity)
+    private readonly boardRepository: Repository<BoardEntity>,
+  ) {}
 
   async getAllBoards() {
     
@@ -9,6 +18,14 @@ export class BoardService {
   }
 
   async createBoard(body: any) {
-    return 'createBoard';
+    const newBoard: BoardEntity = new BoardEntity();
+    newBoard.authorId = body.authorId;
+    newBoard.title = body.title;
+    newBoard.description = body.description;
+    newBoard.content = body.content;
+    newBoard.tags = body.tags;
+    newBoard.status = body.status;
+    
+    return await this.boardRepository.save(newBoard);;
   }
 }
