3ff96f88bb247ae0da562627182ba574936ffc7d
hugo.p <jongyeonpark.dev@gmail.com>
2023-09-28 20:44:09 +0900

    added test for board service method

--- packages/trip-poor-backend/src/board/board.service.spec.ts
+++ packages/trip-poor-backend/src/board/board.service.spec.ts
@@ -2,6 +2,59 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { BoardService } from './board.service';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { BoardEntity } from '../entities/boardEntity.entity';
+import { BoardStatus } from '../utils/enum/boardStatus';
+import { Emotion } from '../utils/enum/emotion';
+import { ApiResponse } from '@nestjs/swagger';
+import { EmotionEntity } from '../entities/emotionEntity.entity';
+
+const mockBoards = [
+  {
+    createdAt: '2023-09-26T12:00:31.012Z' as any,
+    updatedAt: '2023-09-26T12:00:31.012Z' as any,
+    id: 1,
+    authorId: 1,
+    title: 'testtt',
+    description: '',
+    content: 'testContent',
+    tags: '',
+    status: BoardStatus.PUBLIC,
+  },
+  {
+    createdAt: '2023-09-26T12:00:43.694Z' as any,
+    updatedAt: '2023-09-26T12:00:43.694Z' as any,
+    id: 2,
+    authorId: 1,
+    title: 'testtt2',
+    description: '',
+    content: 'testContent2',
+    tags: '["test","testTag"]',
+    status: BoardStatus.PUBLIC,
+  },
+];
+
+const mockEmotions = [
+  {
+    createdAt: '2023-09-26T12:00:31.012Z' as any,
+    updatedAt: '2023-09-26T12:00:31.012Z' as any,
+    boardId: 1,
+    userId: 1,
+    emotion: Emotion.LIKE,
+  },
+  {
+    createdAt: '2023-09-26T12:00:31.012Z' as any,
+    updatedAt: '2023-09-26T12:00:31.012Z' as any,
+    boardId: 1,
+    userId: 2,
+    emotion: Emotion.DISLIKE,
+  },
+  {
+    createdAt: '2023-09-26T12:00:31.012Z' as any,
+    updatedAt: '2023-09-26T12:00:31.012Z' as any,
+    boardId: 1,
+    userId: 3,
+    emotion: Emotion.LIKE,
+  }
+]
 
 describe('BoardService', () => {
   let service: BoardService;
@@ -19,6 +72,15 @@ describe('BoardService', () => {
             delete: jest.fn(),
           },
         },
+        {
+          provide: getRepositoryToken(EmotionEntity),
+          useValue: {
+            find: jest.fn(),
+            findOne: jest.fn(),
+            save: jest.fn(),
+            delete: jest.fn(),
+          },
+        },
       ],
     }).compile();
 
@@ -27,5 +89,33 @@ describe('BoardService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should return all boards', async () => {
+    
+
+    jest.spyOn(service, 'getAllBoards').mockResolvedValue(mockBoards);
+
+    expect(await service.getAllBoards()).toEqual(mockBoards);
+  });
+
+  it('should return one board', async () => {
+    jest.spyOn(service, 'getBoard').mockResolvedValue(mockBoards[1]);
+
+    expect(await service.getBoard(1)).toEqual(mockBoards[1]);
+  });
+
+  it('should create a board', async () => {
+    jest.spyOn(service, 'createBoard').mockResolvedValue(mockBoards[0]);
+
+    expect(await service.createBoard(mockBoards[0])).toEqual(mockBoards[0]);
+  });
+
+  it('should add emotion to a board', async () => {
+    const spy = jest.spyOn(service, 'updateEmotion');
+
+    const result = {code: 200, message: 'add emotion success'};
+
+    expect(await service.updateEmotion(1, 3, Emotion.LIKE)).toEqual(result);
   });
 });
