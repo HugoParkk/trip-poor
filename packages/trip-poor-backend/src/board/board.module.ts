6d8293c340b39b4e08becf82448e27fc73f42387
hugopark <jongyeonpark.dev@gmail.com>
2023-10-12 16:58:04 +0900

    backend board userEntity added

--- packages/trip-poor-backend/src/board/board.module.ts
+++ packages/trip-poor-backend/src/board/board.module.ts
@@ -4,9 +4,10 @@ import { BoardController } from './board.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BoardEntity } from '../entities/boardEntity.entity';
 import { EmotionEntity } from '../entities/emotionEntity.entity';
+import { UserEntity } from 'src/entities/userEntity.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([BoardEntity, EmotionEntity])],
+  imports: [TypeOrmModule.forFeature([BoardEntity, UserEntity, EmotionEntity])],
   providers: [BoardService],
   controllers: [BoardController],
 })
