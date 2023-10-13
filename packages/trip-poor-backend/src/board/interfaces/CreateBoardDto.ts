331851cbfc31ee133e2885a6f36b852b288073aa
hugopark <jongyeonpark.dev@gmail.com>
2023-10-13 16:20:57 +0900

    backend create board class-validation added

--- packages/trip-poor-backend/src/board/interfaces/CreateBoardDto.ts
+++ packages/trip-poor-backend/src/board/interfaces/CreateBoardDto.ts
@@ -1,15 +1,18 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { BoardStatus } from '../../utils/enum/boardStatus';
+import { IsArray, IsEnum, IsString } from 'class-validator';
 
 export class CreateBoardDto {
-  @ApiProperty({ description: '게시글 작성자 고유 번호', default: 1 })
-  authorId: number;
   @ApiProperty({ description: '게시글 제목', default: 'testTitle' })
+  @IsString()
   title: string;
   @ApiProperty({ description: '게시글 설명', default: 'testDescription' })
+  @IsString()
   content: string;
   @ApiProperty({ description: '게시글 태그', default: '["test","testTag"]' })
+  @IsArray()
   tags: string[];
   @ApiProperty({ description: '게시글 상태', default: 'public' })
+  @IsEnum(BoardStatus)
   status: BoardStatus;
 }
