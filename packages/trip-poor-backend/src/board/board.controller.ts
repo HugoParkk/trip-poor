9aa1de5a1c14b6480335f0dd53b02e645abcb532
hugopark <jongyeonpark.dev@gmail.com>
2023-10-16 13:56:07 +0900

    backend boardController added comment crud TODO

--- packages/trip-poor-backend/src/board/board.controller.ts
+++ packages/trip-poor-backend/src/board/board.controller.ts
@@ -7,6 +7,7 @@ import {
   Param,
   Post,
   Put,
+  Query,
   Req,
   Res,
   UseGuards,
@@ -122,4 +123,76 @@ export class BoardController {
     return res.json(await this.boardService.updateEmotion(boardId, userEmail, emotion));
   }
 
+  @ApiOperation({ summary: '게시물 댓글 조회', description: '게시물의 댓글을 조회합니다.' })
+  @ApiResponse({ status: 200, description: 'get comment success', type: ApiResponse })
+  @Get(':id/comment')
+  async getComment(
+    @Req() req: Request,
+    @Res() res: Response,
+    @Param('id') boardId: number,
+  ) {
+    /* TODO: get comment */
+    // return res.json(await this.boardService.getComment(boardId));
+  }
+
+  @ApiOperation({ summary: '게시물에 댓글 추가', description: '게시물에 댓글을 추가합니다.' })
+  @ApiBearerAuth('Authorization')
+  @ApiResponse({ status: 200, description: 'add comment success', type: ApiResponse })
+  @Post(':id/comment')
+  @UseGuards(AuthGuard('jwt'))
+  async addComment(
+    @Req() req: Request,
+    @Res() res: Response,
+    @Param('id') boardId: number,
+    @Body() body: any,
+  ) {
+    const userEmail = req.user.email;
+    const content: string = body.content;
+
+    if (content == null) {
+      return res.json({ code: 400, message: 'content is not valid' });
+    }
+
+    /* TODO: add comment */
+    // return res.json(await this.boardService.addComment(boardId, userEmail, content));
+  }
+
+  @ApiOperation({ summary: '게시물 댓글 수정', description: '게시물의 댓글을 수정합니다.' })
+  @ApiBearerAuth('Authorization')
+  @ApiResponse({ status: 200, description: 'update comment success', type: ApiResponse })
+  @Put(':id/comment')
+  @UseGuards(AuthGuard('jwt'))
+  async updateComment(
+    @Req() req: Request,
+    @Res() res: Response,
+    @Param('id') boardId: number,
+    @Query('commentId') commentId: number,
+    @Body() body: any
+  ) {
+    const userEmail = req.user.email;
+    const content: string = body.content;
+
+    if (content == null) {
+      return res.json({ code: 400, message: 'content is not valid' });
+    }
+
+    /* TODO: update comment */
+    // return res.json(await this.boardService.updateComment(boardId, userEmail, commentId, content));
+  }
+
+  @ApiOperation({ summary: '게시물 댓글 삭제', description: '게시물의 댓글을 삭제합니다.' })
+  @ApiBearerAuth('Authorization')
+  @ApiResponse({ status: 200, description: 'delete comment success', type: ApiResponse })
+  @Delete(':id/comment')
+  @UseGuards(AuthGuard('jwt'))
+  async deleteComment(
+    @Req() req: Request,
+    @Res() res: Response,
+    @Param('id') boardId: number,
+    @Query('commentId') commentId: number,
+  ) {
+    const userEmail = req.user.email;
+    /* TODO: delete comment */
+    // return res.json(await this.boardService.deleteComment(id, userEmail));
+  }
 }
