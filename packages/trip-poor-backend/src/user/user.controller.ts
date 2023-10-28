5c7a457b44d6975457fcc0936fa51538e980f0e6
hugo.p <jongyeonpark.dev@gmail.com>
2023-10-28 16:24:53 +0900

    backend userController todo added

--- packages/trip-poor-backend/src/user/user.controller.ts
+++ packages/trip-poor-backend/src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Req, Res, UseGuards } from '@nestjs/common';
+import { Controller, Get, Put, Req, Res, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { ApiBearerAuth, ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { AuthGuard } from '@nestjs/passport';
@@ -18,4 +18,17 @@ export class UserController {
     // TODO: 유저 프로필 조회
     // return 
   }
+
+  @ApiOperation({ summary: '유저 프로필 수정', description: '유저 프로필 수정'})
+  @ApiBearerAuth('Authorization')
+  @ApiResponse({ status: 200, description: '성공' })
+  @Put()
+  @UseGuards(AuthGuard('jwt'))
+  async updateUserProfile(@Req() req: Request, @Res() res: Response) {
+    
+    // TODO: 유저 프로필 수정
+    // return
+  }
+
+  
 }
